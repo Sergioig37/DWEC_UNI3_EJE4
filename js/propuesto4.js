89b6108c196fc7303c39ea8470c523ed5f0293b3
Sergioig37 <sergioig37@educastur.es>
2023-10-30 14:22:03 +0100

    medio-hecho

--- js/propuesto4.js
+++ js/propuesto4.js
@@ -1,1 +1,21 @@
 
+window.addEventListener("load", init);
+
+function queAnimal(animal){
+    document.getElementById("ultimo").innerHTML = animal;
+}
+
+
+
+function manejadora(e){
+
+}
+
+function aumentarContador(animal){
+    var contador = 0;
+    document.getElementById(animal).c
+}
+
+function int(){
+    document.getElementById("animal").addEventListener("click");
+}
