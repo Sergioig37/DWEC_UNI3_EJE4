a58ad9f738357d123047b0fd824e40e8fce1c286
Sergioig37 <sergioig37@educastur.es>
2023-10-31 10:15:31 +0100

    casi casi, no se que falla

--- js/propuesto4.js
+++ js/propuesto4.js
@@ -1,21 +1,30 @@
+window.addEventListener("load", init); 
 
-window.addEventListener("load", init);
-
-function queAnimal(animal){
-    document.getElementById("ultimo").innerHTML = animal;
+function queAnimal(animal) {
+    document.getElementById("ultimo").innerHTML = "<h1>" +animal +"</h1>";
 }
 
-
-
-function manejadora(e){
-
+function manejadora(e) {
+    if (e.target.class == "perro") {
+        aumentarContador(e.target.class);
+        queAnimal(e.target.class);
+    } else if (e.target.class == "gato") {
+        aumentarContador(e.target.class);
+        queAnimal();
+    }
 }
 
 function aumentarContador(animal){
-    var contador = 0;
-    document.getElementById(animal).c
+    if(document.getElementByClassName(animal).nextSibling != "Sin pulsaciones"){
+        document.getElementByClassName(animal).nextSibling.innerHTML +=1;
+    } 
+    else{
+        document.getElementById(animal).nextSibling = 0;
+    }
 }
 
-function int(){
-    document.getElementById("animal").addEventListener("click");
+function init(){
+    document.getElementsByClassName("perro").addEventListener("click", manejadora(), false);
+    document.getElementsByClassName("gato").addEventListener("click", manejadora(), false);
 }
+ 
